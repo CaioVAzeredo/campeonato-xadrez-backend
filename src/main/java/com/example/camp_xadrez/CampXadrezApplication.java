350c2254161967a9e08ef2f03202a18a12395bff
caiovazeredo <caio.viana.39@gmail.com>
2025-09-15 23:58:50 -0300

    adição das dependencias no pom.xml, application e db

---
+++ src/main/java/com/example/camp_xadrez/CampXadrezApplication.java
@@ -0,0 +1,13 @@
+package com.example.camp_xadrez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampXadrezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampXadrezApplication.class, args);
+	}
+
+}
